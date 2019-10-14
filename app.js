9b340368cf73ddb10415b8c0f9da54261dee5297
katerynashydlovska <shydlovskayaka@gmail.com>
2019-10-14 14:51:28 -0700

    added 3rd pict and views counter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 var leftImageEl = document.getElementById('left');
-// var centertImageEl = document.getElementById('center');
+var centerImageEl = document.getElementById('center');
 var rightImageEl = document.getElementById('right');
 var containerEl = document.getElementById('image_container');
 
@@ -30,30 +30,35 @@ function renderProducts() {
 
   uniquePicsArray[0] = makeRandom();
   uniquePicsArray[1] = makeRandom();
-  // uniquePicsArray[2] = makeRandom();
-  while (uniquePicsArray[0] === uniquePicsArray[1]) {
-  // console.error('Duplicate found, Re-rolling!');
-    uniquePicsArray[1] = makeRandom();
-  }
+  uniquePicsArray[2] = makeRandom();
 
-// while (uniquePicsArray[0] === uniquePicsArray[1] || uniquePicsArray[1] === uniquePicsArray[2] || uniquePicsArray[0] === uniquePicsArray[2]) {
+  // while (uniquePicsArray[0] === uniquePicsArray[1]) {
+  // // console.error('Duplicate found, Re-rolling!');
+    
+  //   uniquePicsArray[1] = makeRandom();
+  // }
 
 
 
   //add views here
-  // allProducts[uniquePicsArray[0]].views++;
+  allProducts[uniquePicsArray[0]].views++;
   //get a random index
   //display a product whose index is the random number
   console.log('EXISTS?', allProducts[uniquePicsArray[0]]);
 
   leftImageEl.src = allProducts[uniquePicsArray[0]].path;
-  // leftImageEl.name = allProducts[uniquePicsArray[0]].name;
+  leftImageEl.name = allProducts[uniquePicsArray[0]].name;
   leftImageEl.title = allProducts[uniquePicsArray[0]].name;
   //add views here
   allProducts[uniquePicsArray[1]].views++;
-  rightImageEl.src = allProducts[uniquePicsArray[1]].path;
-  // rightImageEl.name = allProducts[uniquePicsArray[1]].name;
-  rightImageEl.title = allProducts[uniquePicsArray[1]].name;
+  centerImageEl.src = allProducts[uniquePicsArray[1]].path;
+  centerImageEl.name = allProducts[uniquePicsArray[1]].name;
+  centerImageEl.title = allProducts[uniquePicsArray[1]].name;
+
+  allProducts[uniquePicsArray[2]].views++;
+  rightImageEl.src = allProducts[uniquePicsArray[2]].path;
+  rightImageEl.name = allProducts[uniquePicsArray[2]].name;
+  rightImageEl.title = allProducts[uniquePicsArray[2]].name;
 
 }
 new Product('bag');
